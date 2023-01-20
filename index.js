712a5357998e7d8ae45b84879ba72cfc79af292f
Mevine Teelokee <mevine.teelokee@gmail.com>
2023-01-20 15:28:26 +0100

    testing node

--- index.js
+++ index.js
@@ -1,6 +1,41 @@
-const chalk = require('chalk')
+const chalk = require("chalk");
+const http = require("http");
+const fs = require("fs");
+const axios = require("axios");
+const port = 8000;
+const host = "localhost";
 
-console.log(chalk.blue("HichT"))
-console.log(chalk.yellow("Elyes"))
-console.log(chalk.red("Vahitu"))
-console.log(chalk.white("Agash"))
+const server = http.createServer((request, response) => {
+  response.writeHead(200);
+  response.end("<h1>My first server</h1>");
+});
+
+server.listen(port, host, () => {
+  console.log(`Server is running on http://${host}:${port}`);
+});
+
+fs.writeFile("noodles.txt", "I like noodles", { encoding: "utf-8" }, () => {
+  console.log("data created");
+});
+
+fs.readFile("./noodles.txt", "utf-8", (err, data) => {
+    if(err) throw err
+  console.log(data);
+});
+
+axios
+  .get("https://www.google.com/")
+  .then((response) => {
+    fs.writeFile("res.txt", response.data, (err) => {
+      console.log(response);
+    });
+  })
+  .catch((err) => console.log(err));
+
+
+
+console.log(chalk.blue("HichT"));
+console.log(chalk.yellow("Elyes"));
+console.log(chalk.red("Vahitu"));
+console.log(chalk.white("Agash"));
+
